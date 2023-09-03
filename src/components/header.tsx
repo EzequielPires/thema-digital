44a4337005117c59017a0c520b09385c6a3898d3
EzequielPires <ezequiel.pires082000@gmail.com>
2023-09-03 18:04:56 -0300

    Commit

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -1,14 +1,15 @@
 'use client'
 
-import { Menu } from "lucide-react";
+import { Menu, X } from "lucide-react";
 import Image from "next/image";
 import Link from "next/link";
-import {usePathname} from "next/navigation";
+import { usePathname } from "next/navigation";
 import { useEffect, useState } from "react";
 
 export function Header() {
     const pathname = usePathname();
     const [isTop, setIsTop] = useState(true);
+    const [show, setShow] = useState(false);
 
     useEffect(() => {
         const handleScroll = () => {
@@ -26,7 +27,7 @@ export function Header() {
     return (
         <header className={`bg-zinc-700 ${isTop ? 'lg:bg-transparent' : 'lg:bg-white/10 lg:backdrop-blur-2xl'} fixed top-0 left-0 w-full z-50`}>
             <div className="w-full max-w-7xl mx-auto px-3 lg:px-0 h-16 flex items-center justify-between">
-                <Link href={'/'}><Image src={'/logos/logo_branco.svg'} width={100} height={30} alt="Logo"/></Link>
+                <Link href={'/'}><Image src={'/logos/logo_branco.svg'} width={100} height={30} alt="Logo" /></Link>
                 <ul className="lg:flex gap-10 hidden">
                     <li>
                         <Link href={'/'} className={`text-sm ${pathname === '/' ? 'text-white' : 'text-white/70'} hover:text-white transition-colors`}>Início</Link>
@@ -44,7 +45,33 @@ export function Header() {
                         <Link href={'/blog'} className={`text-sm ${pathname === '/blog' ? 'text-white' : 'text-white/70'} hover:text-white transition-colors`}>Blog</Link>
                     </li>
                 </ul>
-                <button className="flex lg:hidden"><Menu size={32}/></button>
+                <button className="flex lg:hidden" onClick={() => setShow(true)}><Menu size={32} /></button>
+                {show &&
+                    <div className="fixed top-0 left-0 right-0 bottom-0 bg-zinc-900 px-4 py-4">
+                        <div className="flex justify-end mb-4">
+                            <button onClick={() => setShow(false)}>
+                                <X />
+                            </button>
+                        </div>
+                        <ul className="flex flex-col gap-10">
+                            <li>
+                                <Link href={'/'} className={`text-lg ${pathname === '/' ? 'text-white' : 'text-white/70'} hover:text-white transition-colors`}>Início</Link>
+                            </li>
+                            <li>
+                                <Link href={'/sobre'} className={`text-lg ${pathname === '/sobre' ? 'text-white' : 'text-white/70'} hover:text-white transition-colors`}>Sobre</Link>
+                            </li>
+                            <li>
+                                <Link href={'/portifolio'} className={`text-lg ${pathname === '/portifolio' ? 'text-white' : 'text-white/70'} hover:text-white transition-colors`}>Projetos</Link>
+                            </li>
+                            <li>
+                                <Link href={'/contato'} className={`text-lg ${pathname === '/contato' ? 'text-white' : 'text-white/70'} hover:text-white transition-colors`}>Contato</Link>
+                            </li>
+                            <li>
+                                <Link href={'/blog'} className={`text-lg ${pathname === '/blog' ? 'text-white' : 'text-white/70'} hover:text-white transition-colors`}>Blog</Link>
+                            </li>
+                        </ul>
+                    </div>
+                }
             </div>
         </header>
     )
